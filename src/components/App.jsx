6542ac3ed70cbb9aea83f367430de97a3cc23f9e
LysovMaxim <max_maran@icloud.com>
2023-05-18 14:13:16 +0300

    Копіював src з попереднього завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Contacts } from './Contacts';
+import { Filter } from './Filter';
+import { Forma } from './Forma';
+import { nanoid } from 'nanoid';
+import {ContainerForm} from "./App.styled"
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  getVisibleContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  addContacts = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(pravState => {
+      return { contacts: [contact, ...pravState.contacts] };
+    });
+  };
+
+  deleteContact = (contactId) => {
+    this.setState(prevState =>({contacts: prevState.contacts.filter(contact => contact.id !== contactId)})
+      
+    )
+  };
+
+  render() {
+    const visiblContacts = this.getVisibleContacts();
+
+    return (
+      <ContainerForm>
+        <h1>Phonebook</h1>
+        <Forma onSubmit={this.addContacts} arr={this.state.contacts} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <Contacts
+          contacts={visiblContacts}
+          onDeleteContacts={this.deleteContact}
+        />
+      </ContainerForm>
+    );
+  }
+}
